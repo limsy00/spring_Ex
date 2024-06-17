b1a5c0c2c0a957f802ed9162aaf7c9cd64daa358
DESKTOP-L7G5VSR\user <test@test.com>
2024-06-17 19:02:20 +0900

    240617 수업 (lesson03 ex02~ex03)

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -8,7 +8,8 @@ import com.example.lesson03.mapper.ReviewMapper;
 
 @Service
 public class ReviewBO {
-	// ◎6
+	// ● Ex01
+	// ◎6 
 	@Autowired
 	private ReviewMapper reviewMapper;
 	
@@ -18,5 +19,28 @@ public class ReviewBO {
 		// input : int id 받아 옴
 		// output: Review(단건) or null → controller한테 전달 
 		return reviewMapper.selectReviewById(id); // input 넘기기!
+	}
+	
+	// ● Ex02
+	// 1) input: Review, output: 성공한 행의 개수 (int)
+	public int addReview(Review review) {
+		return reviewMapper.insertReview(review);
+	}
+	/* ● 방법2
+	 * public void addReview(Review review) { int count =
+	 * reviewMapper.insertReview(review); }
+	 */
+	
+	// 2) input : 4, "콤비네이션L", "바다", 4.5, "역시 도미노 맛도뤼~"
+	public int addReviewAsField(int storeId, String menu, 
+			String userName, Double point, String review) {
+		return reviewMapper.insertReviewAsField(storeId, menu, userName, point, review);
+	}
+	
+	// ● Ex03
+	// input: id(where 조회 대상), review
+	// output:  성공한 행 개수
+	public int updateReviewById(int id, String review) {
+		return reviewMapper.updateReviewById(id, review);
 	}
  }
--- src/main/java/com/example/lesson03/mapper/ReviewMapper.java
+++ src/main/java/com/example/lesson03/mapper/ReviewMapper.java
@@ -1,13 +1,29 @@
 package com.example.lesson03.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.lesson03.domain.Review;
 
 @Mapper
 public interface ReviewMapper {
-	// ◎4
+	// ●Ex01
 	// input: int id
 	// output: Review() or null 
 	public Review selectReviewById(int id); 
+	
+	// ●Ex02
+	public int insertReview(Review review);
+
+	public int insertReviewAsField(
+			@Param("storeId") int storeId, 
+			@Param("menu") String menu, 
+			@Param("userName") String userName, 
+			@Param("point") Double point, 
+			@Param("review") String review);
+
+	// ●Ex03
+	public int updateReviewById(
+			@Param("id") int id, 
+			@Param("review") String review);
 }
