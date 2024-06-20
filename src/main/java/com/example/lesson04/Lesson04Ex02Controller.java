49d34388d16b2bee5bb85422e0658e4c3089fbab
DESKTOP-L7G5VSR\user <test@test.com>
2024-06-20 18:35:28 +0900

    240620 수업

--- src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
@@ -2,6 +2,7 @@ package com.example.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,15 +27,18 @@ public class Lesson04Ex02Controller {
 	
 	// DB Insert => 방금 가입된 학생 select => 화면
 	@PostMapping("/add-student")
-	public String addStudent(
+	public String addStudent( Model model,
 			@ModelAttribute Student student) { // @RequestParam 한번에 받아오는 방법
 		
 		// DB insert
 		studentBO.addStudent(student);
 		
-		// DB select => 방금 가입된 학생
+		// DB select => 방금 가입된 1명의 학생
+		int id = student.getId();
+		Student latestStudent = studentBO.getStudentById(id);
 		
 		// 데이터 담을 Model 주머니
+		model.addAttribute("student", latestStudent);
 		
 		// 화면 이동
 		return "lesson04/afterAddStudent";
